61c7425e64c8fa96361cde8711fd10c5c358763e
ForgeDom <maxstill2006@gmail.com>
2025-04-01 19:12:05 +0300

    +

---
+++ Class/Subhelper.cs
@@ -0,0 +1,154 @@
+﻿namespace ClassesPract2.Class;
+
+internal class Subhelper
+    {
+        private string _fullName;
+        private string _birthDate;
+        private string _phoneNumber;
+        private string _email;
+        private string _position;
+        private string _duty;
+        private int _salary;
+    
+        public Subhelper()
+        {
+            _fullName = "None";
+            _birthDate = "None";
+            _phoneNumber = "None";
+            _email = "None";
+            _position = "None";
+            _duty = "None";
+            _salary = 0;
+        }
+
+        public Subhelper(string fullName, string birthDate, string phoneNumber, string email, string position, string duty, int salary)
+        {
+            _fullName = fullName;
+            _birthDate = birthDate;
+            _phoneNumber = phoneNumber;
+            _email = email;
+            _position = position;
+            _duty = duty;
+            _salary = salary;
+        }
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set { _fullName = value; }
+        }
+        public string BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        public string Duty
+        {
+            get { return _duty; }
+            set { _duty = value; }
+        }
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter Full Name:");
+            FullName = Console.ReadLine();
+
+            Console.WriteLine("Enter Birth Date (yyyy-mm-dd):");
+            BirthDate = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email:");
+            Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Position:");
+            Position = Console.ReadLine();
+
+            Console.WriteLine("Enter Duty Description:");
+            Duty = Console.ReadLine();
+            
+            Console.WriteLine("Enter Salary:");
+            while (true)
+            {
+                try
+                {
+                    Salary = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid salary.");
+                }
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Employee Information:");
+            Console.WriteLine($"Full Name: {FullName}");
+            Console.WriteLine($"Birth Date: {BirthDate}");
+            Console.WriteLine($"Phone Number: {PhoneNumber}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine($"Duty Description: {Duty}");
+            Console.WriteLine($"Salary: {Salary}");
+        }
+        
+        public static Subhelper operator +(Subhelper subhelper, int salary)
+        {
+            subhelper._salary = salary;
+            return subhelper;
+        }
+        public static Subhelper operator -(Subhelper subhelper, int salary)
+        {
+            subhelper._salary -= salary;
+            return subhelper;
+        }
+        public static Subhelper operator ++(Subhelper subhelper)
+        {
+            subhelper._salary++;
+            return subhelper;
+        }
+        public static Subhelper operator --(Subhelper subhelper)
+        {
+            subhelper._salary--;
+            return subhelper;
+        }
+        public static bool operator ==(Subhelper subhelper1, Subhelper subhelper2)
+        {
+            return subhelper1._salary == subhelper2._salary;
+        }
+
+        public static bool operator !=(Subhelper subhelper1, Subhelper subhelper2)
+        {
+            return !(subhelper1 == subhelper2);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Subhelper subhelper)
+            {
+                return this == subhelper;
+            }
+            return false;
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿namespace ClassesPract2;
+﻿using ClassesPract2.Class;
+namespace ClassesPract2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Subhelper employee1 = new Subhelper("Employee1", "2020-01-01", "1234567890", "my@gmail.com", "Manager", "Manage", 1000);
+        Subhelper employee2 = new Subhelper("Employee2", "2021-01-01", "0987654321", "his@gmail.com", "Boss", "Shout", 2000);
+
+        Console.WriteLine("Employee 1:");
+        employee1.Display();
+        Console.WriteLine("Employee 2:");
+        employee2.Display();
+        
+        Console.WriteLine("Enter the amount to add to Employee 1's salary:");
+        int amountToAdd = int.Parse(Console.ReadLine());
+        employee1.Salary += amountToAdd;
+        Console.WriteLine("Employee 1's salary after addition:");
+        employee1.Display();
+        Console.WriteLine("Enter the amount to subtract from Employee 1's salary:");
+        int amountToSubtract = int.Parse(Console.ReadLine());
+        employee1.Salary -= amountToSubtract;
+        Console.WriteLine("Employee 1's salary after subtraction:");
+        employee1.Display();
+        Console.WriteLine("Incrementing Employee 1's salary:");
+        employee1.Salary++;
+        employee1.Display();
+        Console.WriteLine("Decrementing Employee 1's salary:");
+        employee1.Salary--;
+        employee1.Display();
+        
     }
 }
