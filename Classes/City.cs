462cbecd6189e14f5b3f0d0c94c9ad366732617f
Max <maxstill2006@gmail.com>
2025-04-03 10:53:40 +0300

    +

---
+++ Classes/City.cs
@@ -0,0 +1,126 @@
+﻿namespace ClassesPract2.Classes;
+
+ internal class City
+    {
+        private string _cityName;
+        private string _countryName;
+        private int _cityPopulation;
+        private string _cityCode;
+        private string[] _districts;
+
+        public City()
+        {
+            _cityName = "Empty";
+            _countryName = "Empty";
+            _cityPopulation = 0;
+            _cityCode = "Empty";
+            _districts = new string[0]; 
+        }
+        public City(string cityName, string countryName, int cityPopulation,string cityCode, string[] districts)
+        {
+            _cityName=cityName;
+            _countryName=countryName;
+            _cityPopulation=cityPopulation;
+            _cityCode=cityCode;
+            _districts=districts;
+        }
+        public string CityName
+        {
+            get { return _cityName; }
+            set { _cityName = value; }
+        }
+        public string CountryName
+        {
+            get { return _countryName; }
+            set { _countryName = value; }
+        }
+        public int CityPopulation
+        {
+            get { return _cityPopulation; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Population can`t be less than 0!");
+                }
+                _cityPopulation = value;
+            }
+        }
+        public string CityCode
+        {
+            get { return _cityCode; }
+            set { _cityCode = value; }
+        }
+        public string[] Districts
+        {
+            get { return _districts; }
+            set { _districts = value; }
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter city name:");
+            _cityName = Console.ReadLine();
+
+            Console.WriteLine("Enter country name:");
+            _countryName = Console.ReadLine();
+
+            Console.WriteLine("Enter population:");
+            _cityPopulation = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter phone code:");
+            _cityCode = Console.ReadLine();
+
+            Console.WriteLine("Enter number of districts:");
+            int districtCount = Convert.ToInt32(Console.ReadLine());
+            _districts = new string[districtCount];
+
+            Console.WriteLine("Enter district names:");
+            for (int i = 0; i < districtCount; i++)
+            {
+                Console.Write($"District {i + 1}: ");
+                _districts[i] = Console.ReadLine();
+            }
+        }
+        public void DisplayData()
+        {
+            Console.WriteLine($"City Name: {_cityName}");
+            Console.WriteLine($"Country Name: {_countryName}");
+            Console.WriteLine($"Population: {_cityPopulation}");
+            Console.WriteLine($"Phone Code: {_cityCode}");
+            Console.WriteLine("Districts:");
+            foreach (var district in _districts)
+            {
+                Console.WriteLine($"- {district}");
+            }
+        }
+        
+        public static City operator +(City city, int population)
+        {
+            city.CityPopulation += population;
+            return city;
+        }
+
+        public static City operator -(City city, int population)
+        {
+            city.CityPopulation -= population;
+            return city;
+        }
+
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.CityPopulation == city2.CityPopulation;
+        }
+
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.CityPopulation != city2.CityPopulation;   
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is City city)
+            {
+                return this == city;
+            }
+            return false;
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace ClassesPract2;
+﻿using ClassesPract2.Classes;
+namespace ClassesPract2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        City city1 = new City("CityA", "CountryA", 100000, "CodeA", new string[] { "District1", "District2" });
+        City city2 = new City("CityB", "CountryB", 150000, "CodeB", new string[] { "District3", "District4" });
+
+        Console.WriteLine("City 1 information:");
+        city1.DisplayData();
+        Console.WriteLine("\nCity 2 information:");
+        city2.DisplayData();
+
+        Console.WriteLine("\nAdding 5000 to City 1 population:");
+        city1 += 5000;
+        city1.DisplayData();
+
+        Console.WriteLine("\nSubtracting 3000 from City 2 population:");
+        city2 -= 3000;
+        city2.DisplayData();
+
+        Console.WriteLine($"\nCity 1 == City 2: {city1 == city2}");
+        Console.WriteLine($"City 1 != City 2: {city1 != city2}");
     }
 }
